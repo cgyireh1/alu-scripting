56b4b9fd799a13a274a8ef0dbf3be91e6d746395
cgyireh <c.gyireh@alustudent.com>
2023-07-16 06:59:53 -0700

    API advanced

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+Defines a function to query the Reddit API and print titles of the first 10 hot posts.
+"""
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts for a subreddit.
+    """
+    import requests
+
+    subreddit_URL = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+
+    # Setting the user agent header to identify the client making the API request
+    subreddit_info = requests.get(subreddit_URL,
+                                  headers={"user-agent": "user"},
+                                  allow_redirects=False).json()
+
+    if "data" not in subreddit_info:
+        print("None")
+        return
+
+    children = subreddit_info.get("data").get("children")
+
+    for child in children:
+        print(child.get("data").get("title"))
