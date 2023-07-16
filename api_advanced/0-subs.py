5f7bc286b3c866d3f12d3e7e7317737304a1ca80
cgyireh1 <116649919+cgyireh1@users.noreply.github.com>
2023-07-16 16:52:38 +0200

    edit

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -6,12 +6,12 @@ Defines a function that queries the Reddit API and returns the number of subscri
 
 def number_of_subscribers(subreddit):
     """
-    queries the Reddit API
-    returns the number of subscribers for a given reddit
+    queries the Reddit API, returns the number of subscribers for a given Reddit
     """
     import requests
     subreddit_URL = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
-    # Setting the user agent header to identify the client making the API request
+    
+# Setting the user agent header to identify the client making the API request
 subreddit_info = requests.get(subreddit_URL,
                                   headers={"user-agent": "user"},
                                   allow_redirects=False).json()
